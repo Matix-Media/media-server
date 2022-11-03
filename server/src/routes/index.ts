1af4b30c7c1942abac89ce710659629a2735b8ce
Matix-Media <support@matix-media.net>
2022-11-03 23:51:49 +0100

    feat: :sparkles: added front-end static serving

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance, RegisterOptions } from "fastify";
 import v1 from "./api/v1";
+import fastifyStatic from "@fastify/static";
+import path from "path";
 
 export default function (fastify: FastifyInstance, options: RegisterOptions, done: Function) {
     fastify.addHook("onSend", async (req, res) => {
@@ -13,6 +15,10 @@ export default function (fastify: FastifyInstance, options: RegisterOptions, don
     });
 
     fastify.register(v1, { prefix: "/api/v1" });
+
+    const frontEndAppDir = path.resolve(path.join("../client/dist"));
+    fastify.mediaServer.server.logger.debug("Front end app files located in:", frontEndAppDir);
+    fastify.register(fastifyStatic, { root: frontEndAppDir });
 
     done();
 }
